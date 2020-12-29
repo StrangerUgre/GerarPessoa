c26092e00bfaa4de02aa8c1977bd213117d95fc5
Richard <70394707+bbiswatching@users.noreply.github.com>
2020-12-28 21:37:33 -0300

    Update gerarPessoa.py

--- gerarPessoa.py
+++ gerarPessoa.py
@@ -2,7 +2,7 @@
 nm = input('Please, what is your name?')
 gnd = int(input('What\'s your gender?/c/c[1] - Lady/c/c[2] - Gentlemen/c/c'))
   # CHOSE
-  if (gnd == 1)
+  if(gnd == 1)
     print('Hello', 'lady' nm, '!', ' This is a test.')
   else
     print('Hello', 'gentlemen', '!', 'This is a test.')
