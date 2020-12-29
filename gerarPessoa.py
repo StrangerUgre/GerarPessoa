bdcf5e681a5507e98c89c4f99bf320357eeaf0ba
Richard <70394707+bbiswatching@users.noreply.github.com>
2020-12-28 21:16:57 -0300

    Update gerarPessoa.py

--- gerarPessoa.py
+++ gerarPessoa.py
@@ -1,1 +1,5 @@
-print("Teste")
+# DATA INSERTION
+nm = input('Please, what\'s your name?')
+gnd = int(input('What\'s your gender?\c\c\t1 - Lady\c\c2 - Gentlemen'))
+
+print('Hello', gnd, nm, '!', ' This is a test.')
