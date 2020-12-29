81a295fbbb75cf2f7c57d4867877951688dac0f6
Richard <70394707+bbiswatching@users.noreply.github.com>
2020-12-28 21:24:31 -0300

    Update gerarPessoa.py

--- gerarPessoa.py
+++ gerarPessoa.py
@@ -1,5 +1,8 @@
 # DATA INSERTION
 nm = input('Please, what\'s your name?')
-gnd = int(input('What\'s your gender?\c\c\t1 - Lady\c\c2 - Gentlemen'))
-
-print('Hello', gnd, nm, '!', ' This is a test.')
+gnd = int(input('What\'s your gender?/c/c[1] - Lady/c/c[2] - Gentlemen/c/c')):
+  # CHOSE
+  if (gnd == 1):
+    print('Hello', 'lady' nm, '!', ' This is a test.')
+  else:
+    print('Hello', 'gentlemen', '!', 'This is a test.')
